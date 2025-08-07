d9320edfd3e68e821997bbb6027e1094889e7ada
yesmannow <hoosierdarling@gmail.com>
2025-08-07 12:02:42 -0400

    [dyad] Fixed React 18 createRoot import and usage - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import './globals.css';
 import 'leaflet/dist/leaflet.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root')!);
-root.render(
+createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
