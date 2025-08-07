f849d9bdb3a8601b307c551dc0fff0cc3df62596
yesmannow <hoosierdarling@gmail.com>
2025-08-07 12:07:08 -0400

    [dyad] Switched main.tsx back to ReactDOM.render to eliminate render2 error - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 import './globals.css';
 import 'leaflet/dist/leaflet.css';
 
-createRoot(document.getElementById('root')!).render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
