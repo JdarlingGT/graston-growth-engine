fc4db3ac8ff4403826b5f753d375b3902dca84ad
yesmannow <hoosierdarling@gmail.com>
2025-08-12 19:09:04 -0400

    [dyad] wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "@/components/layout/Layout";
-import ProviderDetail from "@/pages/ProviderDetail";
 import Index from "@/pages/Index";
+import About from "@/pages/About";
+import ProviderDetail from "@/pages/ProviderDetail";
 import ProtectedRoute from "@/components/ProtectedRoute";
 import AdminLayout from "@/components/layout/AdminLayout";
 import AdminPage from "@/pages/admin/DashboardPage";
@@ -16,6 +17,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Index />} />
+          <Route path="about" element={<About />} />
           <Route path="providers/:providerId" element={<ProviderDetail />} />
           <Route path="admin" element={<ProtectedRoute><AdminLayout /></ProtectedRoute>}>
             <Route index element={<AdminPage />} />
